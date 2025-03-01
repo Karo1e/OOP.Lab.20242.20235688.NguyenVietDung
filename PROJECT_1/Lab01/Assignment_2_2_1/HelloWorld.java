aaf973ac01844b04ac3a83f4891d70519ce6e0cd
NguyenVIetDung <hollowerbe@gmail.com>
2025-03-01 21:30:24 +0700

    Complete assignment 2.2.1

---
+++ PROJECT_1/Lab01/Assignment_2_2_1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args) {	
+		System.out.println("20235688 - Nguyen Viet Dung");
+		System.out.println("Xin chao\n cac ban!");
+		System.out.println("Hello \t World!");
+		int i = 5, j = 10;
+		System.out.println(i + j);
+	}
+}
