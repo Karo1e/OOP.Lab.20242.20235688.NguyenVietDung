02b3dc0c753b56770e92eca9dc292bfcb9eb1ef0
NguyenVietDung <hollowerbe@gmail.com>
2025-03-01 10:57:04 +0700

    add PROJECT_1

---
+++ PROJECT_1/src/Halo.java
@@ -0,0 +1,15 @@
+
+public class Halo {
+	public static void main(String[] args) {
+		int i = 5;
+		if(i != 5) {
+			System.out.println("i khac 5");
+		} else {
+			System.out.println("i bang 5");
+		}
+		int j = 10;
+		System.out.println(i + j);
+		int k = -2;
+		System.out.println(i + j + k);
+	}
+}
